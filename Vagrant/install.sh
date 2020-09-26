079ee8bece0d50abfc300ce55175cbdeab892762
WuTolas <36102576+WuTolas@users.noreply.github.com>
2020-09-26 23:31:00 +0200

    #32 init vagrant

---
+++ Vagrant/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DBHOST=localhost
+DBNAME=gamepetto
+DBROOTPASSWORD=yeah_root_password
+DBUSER=gamepetto
+DBPASSWORD=development_random_password
+GAMEPETTO_BACK_PROFILE=dev
+
+apt-get update
+apt-get install --yes mariadb-server
+/etc/init.d/mysql restart
+
+mysql -u root -e "CREATE DATABASE $DBNAME;"
+mysql -u root -e "CREATE USER '$DBUSER'@'$DBHOST' IDENTIFIED BY '$DBPASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON $DBNAME.* TO '$DBUSER'@'$DBHOST'; FLUSH PRIVILEGES;"
